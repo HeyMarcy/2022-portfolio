6eb491b5a73a50474190a9ba07d8220fd6008a20
Marcy Montross <marcy.montross@nerdery.com>
2022-10-15 11:17:10 -0500

    adds deps

---
+++ constants.ts
@@ -0,0 +1,30 @@
+export const METADATA = {
+  title: "Marcy Montross | Porfolio",
+  description: "Designing and developing sites for nearly 10 years.",
+  steUrl: "https://heymarcy.com/",
+};
+
+export const MENULINKS = [
+  {
+    name: "Home",
+    ref: "home",
+  },
+  {
+    name: "Projects",
+    ref: "work",
+  },
+  {
+    name: "Skills",
+    ref: "skills",
+  },
+  {
+    name: "Timeline",
+    ref: "timeline",
+  },
+  {
+    name: "Contact",
+    ref: "contact",
+  },
+];
+
+export const EMAIL = "marcy.montross@gmail.com";
